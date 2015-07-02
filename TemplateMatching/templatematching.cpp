aaa8711c70b33507ae44f5f411ce365eebe5e457
kurepasu0731 <kurepasu0731@gmail.com>
2015-07-02 15:18:07 +0900

    テンプレートマッチングdll
    dll作成

---
+++ TemplateMatching/templatematching.cpp
@@ -0,0 +1,32 @@
+﻿#include "templatematching.h"
+
+DLLExport void templateMatching(std::string tmp_img_filename, std::string search_img_filename, double thresh, int& x, int& y)
+{
+	// 探索画像
+	cv::Mat search_img = cv::imread(search_img_filename, 1);
+		 
+	// テンプレート画像
+	cv::Mat tmp_img = cv::imread(tmp_img_filename, 1);
+		 
+	cv::Mat result_img;
+	// テンプレートマッチング
+	cv::matchTemplate(search_img, tmp_img, result_img, CV_TM_CCOEFF_NORMED);
+
+	// 最大のスコアの場所を探す
+	//cv::Rect roi_rect(0, 0, tmp_img.cols, tmp_img.rows);
+	cv::Point max_pt;
+	double maxVal;
+	cv::minMaxLoc(result_img, NULL, &maxVal, NULL, &max_pt);
+	//roi_rect.x = max_pt.x;
+	//roi_rect.y = max_pt.y;
+	std::cout << "(" << max_pt.x << ", " << max_pt.y << "), score=" << maxVal << std::endl;
+	// 探索結果の場所に矩形を描画
+	if(maxVal > thresh){
+		x = max_pt.x;
+		y = max_pt.y;
+	}else{
+		//閾値以下ならエラー値-1を入れておく
+		x = -1;
+		y = -1;
+	}
+}
---
+++ TemplateMatching/templatematching.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <opencv2/opencv.hpp>
+
+#define DLLExport __declspec (dllexport)
+
+
+using namespace cv;
+
+
+extern "C" {
+	DLLExport void templateMatching(std::string tmp_img_filename, std::string search_img_filename, double thresh, int& x, int& y);
+}
